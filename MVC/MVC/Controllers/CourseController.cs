3303d3404bf2e33056ce40750f5ebd9be76db86d
Tharindu Kavishna <kavishna@incubatelabs.com>
2021-09-29 20:48:13 +0530

    MVC pattern in ASP.NET

---
+++ MVC/MVC/Controllers/CourseController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC.Controllers
+{
+    public class CourseController : Controller
+    {
+        public IActionResult Index()
+        {
+            var course = new List<Course>();
+            for (int i = 1; i < 11; i++)
+            {
+                course.Add(new Course()
+                {
+                    Id= i,
+                    Title = $"Course name {i}",
+                    Price = "2000",
+                    //18 52
+                    
+                });
+            }
+            return View(course);
+        }
+    }
+}
---
+++ MVC/MVC/Models/Course.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC.Models
+{
+    public class Course
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Price { get; set; }
+    }
+}
